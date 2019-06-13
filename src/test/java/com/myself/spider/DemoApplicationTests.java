0c4253cda1c93a4985fe8de49531bd7a84d54e80
Holeski <huleski@gmail.com>
2019-06-13 21:09:18 +0800

    first commit

---
+++ src/test/java/com/myself/spider/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myself.spider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
