29d954d7b81b1848e5d74b1a4b464ee6c34f6040
Holeski <huleski@gmail.com>
2019-06-14 20:50:59 +0800

    update

--- src/main/java/com/myself/spider/Demo.java
+++ src/main/java/com/myself/spider/Demo.java
@@ -6,19 +6,27 @@ import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class Demo implements PageProcessor {
+    // 部分一：抓取网站的相关配置，包括编码、抓取间隔(1秒)、重试次数(3次)等
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
-
+    /**
+     * process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
+     *
+     * @param page
+     */
     @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+        // 部分二：定义如何抽取页面信息，并保存下来
         page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
         page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
-        if (page.getResultItems().get("name")==null){
+        if (page.getResultItems().get("name") == null) {
             //skip this page
             page.setSkip(true);
         }
         page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+
+        // 部分三：从页面发现后续的url地址来抓取
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
     }
 
     @Override
@@ -27,6 +35,12 @@ public class Demo implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new Demo()).addUrl("https://github.com/code4craft").thread(5).run();
+        Spider.create(new Demo())
+                //从"https://github.com/code4craft"开始抓
+                .addUrl("https://github.com/code4craft")
+                //开启5个线程抓取
+                .thread(5)
+                //启动爬虫
+                .run();
     }
 }
